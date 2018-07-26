77bed642e190cabefe1310d2bc7e8c837c39c8ed
MLPA-IT <github@mlpa.com.au>
2018-07-26 21:53:34 +0930

    main.js update

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 // MLPA - part attribution to Marcus from Vaadin for concept/code 
 const apiKey = '8b84f82ba0fa4090815f67193925a919';
 const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'abc-news-au';
 
-window.addEventListener('load', e => {
+window.addEventListener('load', async e => {
     updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
 }); 
 
-async function updateNews() {
-    const resp = await fetch(`https://newsapi.org/v2/top-headlines?sources=associated-press&apiKey=${apiKey}`);
+async function updateSources() {
+    const resp = await fetch(`https://newsapi.org/v2/sources?country=au&apiKey=${apiKey}`);
+    const json = await resp.json(); 
+
+    sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n ');
+}
+
+async function updateNews(source = defaultSource) {
+    const resp = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`);
     const json = await resp.json();
     
     main.innerHTML = json.articles.map(createArticle).join('\n ');
@@ -15,12 +30,13 @@ async function updateNews() {
 
 function createArticle(article){
     return `
+    <center>
     <div class="article">
      <a href="${article.url}">
       <h2>${article.title}</h2>
       <img src="${article.urlToImage}">
-      <p></p>
      </a>
      </div>
+     </center>
     `;
 }
