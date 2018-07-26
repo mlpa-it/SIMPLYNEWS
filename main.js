3ce4addfb8c851f59d042cdf21192f6d3defe425
MLPA-IT <github@mlpa.com.au>
2018-07-26 22:14:18 +0930

    main.js update

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ window.addEventListener('load', async e => {
 }); 
 
 async function updateSources() {
-    const resp = await fetch(`https://newsapi.org/v2/sources?country=au&apiKey=${apiKey}`);
+//    const resp = await fetch(`https://newsapi.org/v2/sources?country=au&apiKey=${apiKey}`);
+    const resp = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
     const json = await resp.json(); 
 
     sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n ');
@@ -40,3 +41,4 @@ function createArticle(article){
      </center>
     `;
 }
+
